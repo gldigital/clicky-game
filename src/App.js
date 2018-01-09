3303ba9affcb98484c06a9f998e794d4b9a4e0c9
gldigital <greg@gl-digital.com>
2018-01-09 16:01:58 -0600

    randomizing images on click

--- src/App.js
+++ src/App.js
@@ -8,14 +8,35 @@ import "./App.css";
 class App extends Component {
 
   // Setting this.state.friends to the friends json array
-  state = {
-    friends,
-    count: 0
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends,
+      count: 0,
+      clicked: []
+    };
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.removeFriend = this.removeFriend.bind(this);
+  }
 
    // handleIncrement increments this.state.count by 1
-   handleIncrement = () => {
-    
+   handleIncrement = (id) => {
+    console.log("ID!!", id );
+    console.log(this.state.clicked, "was this clicked!!");
+    var index = this.state.clicked.indexOf(id);
+    console.log("index Of", index);
+
+    // if (index >= 0) {
+    //   alert("You lost");
+    // } else {
+    //   var array = this.state.clicked;
+      
+    //   var newArray = array.push(id);
+      
+    //   this.setState({ clicked: newArray});
+      
+    // }
+
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -23,19 +44,46 @@ class App extends Component {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    // this.setState({ friends });
   };
 
   
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
+    console.log("this state friends!!!", this.state.friends);
+
+    var testArray = ["a", "b", "c", "d"];
+
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+    
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+    
+      return array;
+    }
+
+    var shuffledArray = shuffle(this.state.friends);
+    console.log("shuffled array", shuffledArray);
+
+
     return (
       
       <div className="container-fluid">
            <Nav numClicks={this.state.count}></Nav>
           <div className="container">
             <Wrapper>
-              {this.state.friends.map(friend => (
+              {shuffledArray.map(friend => (
                   <FriendCard
                     key={friend.id}
                     handleIncrement={this.handleIncrement}
